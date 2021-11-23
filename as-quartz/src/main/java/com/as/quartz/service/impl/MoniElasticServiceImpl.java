58924ebd989f2971511b4dbdc0bd685f0d69ddd6
mickey <mickey.lai@yxunistar.com>
2021-11-23 17:44:31 +0800

    修改开奖号码比对格式

--- as-quartz/src/main/java/com/as/quartz/service/impl/MoniElasticServiceImpl.java
+++ as-quartz/src/main/java/com/as/quartz/service/impl/MoniElasticServiceImpl.java
@@ -408,7 +408,7 @@ public class MoniElasticServiceImpl implements IMoniElasticService {
                 JSONObject jsonObject = JSONObject.parseObject(sourceAsString);
                 JSONObject json = jsonObject.getJSONObject("json");
                 String message = json.getString("message");
-                String replace = message.replace("[craw] get Single winNo result:", "");
+                String replace = message.substring(message.indexOf("{"));
                 JSONObject drawInfo = JSONObject.parseObject(replace);
                 String winNo = drawInfo.getString("winningNumber");
                 String numero = drawInfo.getString("numero");
