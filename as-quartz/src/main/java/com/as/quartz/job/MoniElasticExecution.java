37457240999d5da09495faa0ae7ebad0a287aef0
mickey <mickey.lai@yxunistar.com>
2022-01-04 14:58:59 +0800

    優化代碼

--- as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
+++ as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
@@ -85,12 +85,12 @@ public class MoniElasticExecution extends AbstractQuartzJob {
     @Override
     protected void doExecute(JobExecutionContext context, Object job) throws Exception {
         SearchHit[] hits = null;
-        JSONObject urlBody = null;
+        JSONObject urlJSON = null;
         String total = null;
         //新增判斷URL Kibana
         if(Constants.PLATFORM_JY8.equals(moniElastic.getPlatform()) || Constants.PLATFORM_PAYUB8.equals(moniElastic.getPlatform())){
-            urlBody = SpringUtils.getBean(IMoniElasticService.class).doURLElasticSearch(moniElastic);
-            total = urlBody.getJSONObject("result").getJSONObject("rawResponse").getJSONObject("hits").getString("total");
+            urlJSON = SpringUtils.getBean(IMoniElasticService.class).doURLElasticSearch(moniElastic);
+            total = urlJSON.getJSONObject("result").getJSONObject("rawResponse").getJSONObject("hits").getString("total");
         }else{
             SearchResponse searchResponse = SpringUtils.getBean(IMoniElasticService.class).doElasticSearch(moniElastic);
             hits = searchResponse.getHits().getHits();
@@ -118,7 +118,7 @@ public class MoniElasticExecution extends AbstractQuartzJob {
             }
         } else if ((Constants.PLATFORM_JY8.equals(moniElastic.getPlatform()) || Constants.PLATFORM_PAYUB8.equals(moniElastic.getPlatform())) && doMatch(null,result)) {
             //处理需要导出某字段信息
-            saveUrlExportField(urlBody,total);
+            saveUrlExportField(urlJSON,total);
             checkAndAlert();
         }else {
             moniElasticLog.setStatus(Constants.SUCCESS);
@@ -193,11 +193,11 @@ public class MoniElasticExecution extends AbstractQuartzJob {
         }
     }
 
-    private void saveUrlExportField(JSONObject object,String total) {
+    private void saveUrlExportField(JSONObject urlJSON,String total) {
         if(Integer.parseInt(total)>0 && StringUtils.isNotEmpty(moniElastic.getExportField())) {
             String[] exportFields = moniElastic.getExportField().split(",");
             StringBuilder exportResult = new StringBuilder();
-            JSONObject jsonObject = object.getJSONObject("result").getJSONObject("rawResponse").getJSONObject("hits").getJSONArray("hits").getJSONObject(0).getJSONObject("fields");
+            JSONObject jsonObject = urlJSON.getJSONObject("result").getJSONObject("rawResponse").getJSONObject("hits").getJSONArray("hits").getJSONObject(0).getJSONObject("fields");
             int count = 1;
             for (String exportField : exportFields) {
                 exportResult.append("(").append(count).append(")");
