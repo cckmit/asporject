5f5c4db1622cd49b7763e117bb1676c8564c24ca
mickey <mickey.lai@yxunistar.com>
2022-01-18 14:55:49 +0800

    修bug

--- as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
+++ as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
@@ -257,7 +257,7 @@ public class MoniElasticExecution extends AbstractQuartzJob {
         String index = StringUtils.isNull(compareResult) ? "0" : compareResult.get("index");
         Boolean boolExpect = StringUtils.isNull(compareResult.get("ExpectedResult"));
         //输出日志
-        if(!boolExpect){
+        if(!boolExpect && !StringUtils.isNull(compareResult.get("result"))){
             log.info("JY8 - id:"+moniElasticLog.getId()+" result : "+compareResult.get("result"));
         }
         if (boolExpect && !"0".equals(index)) {
@@ -269,7 +269,11 @@ public class MoniElasticExecution extends AbstractQuartzJob {
             moniElasticLog.setExecuteResult(result);
             checkAndAlert();
         }else {
-            result = boolExpect==false?(String.format("find %s hits;\n", index)+compareResult.get("result")):"find 0 hits";
+            if(!boolExpect && !StringUtils.isNull(compareResult.get("result"))){
+                result = (String.format("find %s hits;\n", index)+compareResult.get("result"));
+            }else{
+                result = "find 0 hits";
+            }
             moniElasticLog.setExecuteResult(result);
             moniElasticLog.setStatus(Constants.SUCCESS);
             moniElasticLog.setAlertStatus(Constants.FAIL);
