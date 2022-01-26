bf122e665467690e4a0c609eab83ae92b0996de2
mickey <mickey.lai@yxunistar.com>
2022-01-26 15:29:48 +0800

    優化代碼

--- as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
+++ as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
@@ -129,7 +129,7 @@ public class MoniElasticExecution extends AbstractQuartzJob {
             //处理需要导出某字段信息
             saveUrlExportField(urlJSON,total);
             //幣安數據判斷
-            if(Integer.parseInt(total)>0 && (moniElastic.getId()==77 || moniElastic.getId()==78)){
+            if(moniElastic.getId()==77 || moniElastic.getId()==78){
                 Map<String, String> compareResult = SpringUtils.getBean(IMoniElasticService.class).doJY8DrawCompare(urlJSON);
                 if(compareResult.get("lists").equals("0")){
                     moniElasticLog.setStatus(Constants.ERROR);
