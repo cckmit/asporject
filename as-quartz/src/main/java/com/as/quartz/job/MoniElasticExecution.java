12e8f568745722dc396f07352ab6fe1433b605aa
mickey <mickey.lai@yxunistar.com>
2022-01-27 14:21:49 +0800

    add log

--- as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
+++ as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
@@ -89,6 +89,7 @@ public class MoniElasticExecution extends AbstractQuartzJob {
             JSONObject urlJSON = SpringUtils.getBean(IMoniElasticService.class).doURLElasticSearch(moniElastic).getJSONObject("result").getJSONObject("rawResponse").getJSONObject("hits");
 
             if((moniElastic.getId()==77 || moniElastic.getId()==78) && urlJSON.getJSONArray("hits").size()==0){
+                log.info("BA SIZE 0:"+urlJSON+" LOGID:"+moniElasticLog.getId());
                 for(int i=1;i<=3;i++){
                     urlJSON = SpringUtils.getBean(IMoniElasticService.class).doURLElasticSearch(moniElastic).getJSONObject("result").getJSONObject("rawResponse").getJSONObject("hits");
                     log.info("BA AGAIN:"+i+" LOGID:"+moniElasticLog.getId()+" SIZE:"+urlJSON.getJSONArray("hits").size());
