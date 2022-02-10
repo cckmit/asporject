2d80778b8248832a6369572a3f37a18c77aac051
mickey <mickey.lai@yxunistar.com>
2022-02-10 14:25:42 +0800

    修改迴圈

--- as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
+++ as-quartz/src/main/java/com/as/quartz/job/MoniElasticExecution.java
@@ -90,11 +90,12 @@ public class MoniElasticExecution extends AbstractQuartzJob {
 
             if((moniElastic.getId()==77 || moniElastic.getId()==78) && urlJSON.getJSONArray("hits").size()==0){
                 log.error("BA SIZE 0:"+urlJSON+" LOGID:"+moniElasticLog.getId());
-                for(int i=1;i<=5;i++){
-                    if(i==3 || i==5)Thread.sleep(1000);
+                int i=1;
+                while(urlJSON.getJSONArray("hits").size()==0){
                     urlJSON = SpringUtils.getBean(IMoniElasticService.class).doURLElasticSearch(moniElastic).getJSONObject("result").getJSONObject("rawResponse").getJSONObject("hits");
                     log.error("BA AGAIN:"+i+" LOGID:"+moniElasticLog.getId()+" SIZE:"+urlJSON.getJSONArray("hits").size());
-                    if(urlJSON.getJSONArray("hits").size()>0) break;
+                    i++;
+                    if(i==10)break;
                 }
             }
             String total = urlJSON.getString("total");
